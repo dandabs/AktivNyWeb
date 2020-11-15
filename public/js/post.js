79e8257475dc42b6281f38dcf5bb81bb1dfc8d94
Daniel Adams <danstanswinxp@gmail.com>
2020-11-15 18:12:22 +0000

    Post update

---
+++ public/js/post.js
@@ -0,0 +1,20 @@
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
+function postPost(uid) {
+
+    firebase.firestore()
+    .collection('posts').doc(uuidv4()).set(
+        {
+            author: firebase.firestore().doc('users/' + uid),
+            body: document.getElementById('editor').innerHTML,
+            timestamp: Math.round(new Date().getTime()/1000),
+            title: document.getElementById('title').innerText
+        }
+    )
+
+}
